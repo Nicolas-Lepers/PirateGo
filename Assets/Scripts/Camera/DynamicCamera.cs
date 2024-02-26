0e102b041a31f0527a4e20704d20ef587b4d0831
P3D\nicolas.lepers <nicolaslepers178@gmail.com>
2024-02-26 17:36:41 +0100

    camera

--- Assets/Scripts/Camera/DynamicCamera.cs
+++ Assets/Scripts/Camera/DynamicCamera.cs
@@ -17,7 +17,7 @@ public class DynamicCamera : MonoBehaviour
     {
         Vector3 targetPos = target.position + offset;
         Vector3 smoothFollow = Vector3.Lerp(transform.position,
-            targetPos, smoothSpeed);
+            targetPos, smoothSpeed * Time.deltaTime);
 
         transform.position = smoothFollow;
     }
